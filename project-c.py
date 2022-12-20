c712423f3e8e909709a4067db512a2c023a9910d
George Sepetadelis <63394215+georgesepetadelis@users.noreply.github.com>
2022-12-20 10:19:25 +0200

    Update project-c.py

--- project-c.py
+++ project-c.py
@@ -1,25 +1,19 @@
-# Customer list program
-
-# Function to create a new customer
 def create_customer():
   name = raw_input("Enter the customer's name: ")
   afm = raw_input("Enter the customer's AFM: ")
   address = raw_input("Enter the customer's address: ")
   phone = raw_input("Enter the customer's phone: ")
 
-  # Add the new customer to the customers.txt file
   with open("customers.txt", "a") as f:
     f.write("{},{},{},{}\n".format(name, afm, address, phone))
 
-# Function to update a customer's information by AFM
 def update_customer():
   afm = raw_input("Enter the AFM of the customer you want to update: ")
 
-  # Read all the lines in the customers.txt file
   with open("customers.txt", "r") as f:
+      
     lines = f.readlines()
 
-  # Find the line with the matching AFM and update it
   found = False
   with open("customers.txt", "w") as f:
     for line in lines:
@@ -33,49 +27,44 @@ def update_customer():
         f.write(line)
 
   if not found:
-    print("No customer with AFM {} was found.".format(afm))
+    print "No customer with AFM {} was found.", afm
 
-# Function to show a customer's information and transactions
 def show_customer():
   afm = raw_input("Enter the AFM of the customer you want to see: ")
 
-  # Read all the lines in the customers.txt file
   with open("customers.txt", "r") as f:
     lines = f.readlines()
 
-  # Find the line with the matching AFM and print it
   found = False
   for line in lines:
     if line.split(",")[1] == afm:
       name, afm, address, phone = line.split(",")
-      print("Name: {}".format(name))
-      print("AFM: {}".format(afm))
-      print("Address: {}".format(address))
-      print("Phone: {}".format(phone))
+      print "Name: ", name
+      print "AFM: ", afm
+      print "Address: ", address
+      print "Phone: ", phone
       found = True
       break
 
   if not found:
-    print("No customer with AFM {} was found.".format(afm))
+    print "No customer with this AFM was found."
 
-  # Read all the lines in the transactions.txt file
   with open("transactions.txt", "r") as f:
     lines = f.readlines()
 
-  # Find all the transactions for the customer with the matching AFM and print them
-  print("Transactions:")
+  print "Transactions:"
   for line in lines:
     if line.split(",")[0] == afm:
-      date, name, afm, balance = line.split(",")
-      print("{} - {} - {} - {}".format(date, name, afm, balance))
-
-# Main function
+        afm, date, name, balance = line.split(",")
+        print date + " " + name + " " + afm + " " + balance
+      #print "- " date, " ", name, " ", afm, " ", balance #.format(date, name, afm, balance))
+        
 def main():
   while True:
-    print("1. Create a new customer")
-    print("2. Update a customer's information")
-    print("3. Show a customer's information and transactions")
-    print("4. Quit")
+    print "1. Create a new customer"
+    print "2. Update a customer's information"
+    print "3. Show a customer's information and transactions"
+    print "4. Quit"
     option = raw_input("Enter an option: ")
 
     if option == "1":
@@ -87,7 +76,6 @@ def main():
     elif option == "4":
       break
     else:
-      print("Invalid option. Please try again.")
+      print "Invalid option. Please try again."
 
-# Call the main function
 main()
