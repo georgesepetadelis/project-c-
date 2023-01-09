6fba73eac28ec45d22631a983f054b66973110c0
George Sepetadelis <63394215+georgesepetadelis@users.noreply.github.com>
2023-01-09 10:40:18 +0200

    Add: Add new transaction option when showing customer info

--- project-c.py
+++ project-c.py
@@ -7,6 +7,10 @@ def create_customer():
   with open("customers.txt", "a") as f:
     f.write("{},{},{},{}\n".format(name, afm, address, phone))
 
+  print ""
+  print "=== CUSTOMER SUCCESSFULLY CREATED ==="
+  print ""
+
 def update_customer():
   afm = raw_input("Enter the AFM of the customer you want to update: ")
 
@@ -25,9 +29,28 @@ def update_customer():
         found = True
       else:
         f.write(line)
+    print ""
+    print "== CUSTOMER UPDATED =="
+    print ""
 
   if not found:
-    print "No customer with AFM {} was found.", afm
+    print ""
+    print "== No customer with AFM {} was found. ==", afm
+    print ""
+
+def create_transaction(cust_afm):
+  afm = cust_afm
+  date = raw_input("Enter the date of the transaction: ")
+  name = raw_input("Enter the name of the transaction: ")
+  balance = raw_input("Enter the balance of the transaction: ")
+
+  with open("transactions.txt", "a") as f:
+    f.write("\n{},{},{},{}\n".format(afm, date, name, balance))
+
+  print ""
+  print "=== TRANSACTION SUCCESSFULLY CREATED ==="
+  print ""
+  main()
 
 def show_customer():
   afm = raw_input("Enter the AFM of the customer you want to see: ")
@@ -39,6 +62,8 @@ def show_customer():
   for line in lines:
     if line.split(",")[1] == afm:
       name, afm, address, phone = line.split(",")
+      print ""
+      print "=== CUSTOMER INFO ==="
       print "Name: ", name
       print "AFM: ", afm
       print "Address: ", address
@@ -52,12 +77,20 @@ def show_customer():
   with open("transactions.txt", "r") as f:
     lines = f.readlines()
 
-  print "Transactions:"
+  print ""
+  print "=== Transactions ==="
   for line in lines:
     if line.split(",")[0] == afm:
         afm, date, name, balance = line.split(",")
         print date + " " + name + " " + afm + " " + balance
-      #print "- " date, " ", name, " ", afm, " ", balance #.format(date, name, afm, balance))
+  print ""
+  print "1. Make new transaction"
+  print "2. Quit"
+  option = input("Enter an option:")
+  if option == 2:
+    main()
+  else:
+    create_transaction(afm)
         
 def main():
   while True:
@@ -76,6 +109,8 @@ def main():
     elif option == "4":
       break
     else:
-      print "Invalid option. Please try again."
+      print ""
+      print "== Invalid option. Please try again. =="
+      print ""
 
 main()
